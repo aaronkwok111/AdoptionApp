86fd2804b0a5514aafffabeb39b0c9bda78adaa3
Matt James <matt.james1310@gmail.com>
2019-11-26 01:29:51 -0800

    Added API functionality

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,40 @@
+var myApiKey = 'aLabKP2phRjIyjMWUlaheDWjoQ9Hs3EpaEWW6SFWtv2GvEX8JC',
+	myApiSecret = 'y5Nto5yodPXbQ37b5GBY3Lyskx0LeoZ2zpdTSDOw';
+
+// declare and initialize petFinder client side API
+var pf = new petfinder.Client({apiKey: myApiKey, secret: myApiSecret});
+$(document).ready(function(){
+
+	// bind onClick function to search button
+	$('#btn_search').click(function(e){
+		console.log("search button pressed");
+
+		// read and store text field value
+		var myLocation = $('#tf_search').val();
+		console.log(myLocation);
+
+		// set up search parameters as JSON object, refer to API docs for
+		// additional search parameters
+		var params = {
+			type: 'dog',
+			location: myLocation,
+			distance: 10
+		}
+
+		// call PetFinder API search function
+		pf.animal.search(params)
+			.then(function (response) {
+    		// response from server with results will be given as an array of JSON objects
+    		console.log(response.data.animals);
+
+    		// replace text in result field with response
+    		$('#result').text(JSON.stringify(response.data.animals));
+		})
+			.catch(function (error) {
+    		// handle any errors
+		});
+	});
+});
+
+
 
